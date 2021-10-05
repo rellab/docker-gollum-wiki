4022b45484c574b9fb8286ebca101fd61a7ef741
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2021-10-05 00:47:40 +0000

    change

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,11 @@
 
 git config --global user.name "${GOLLUM_USER}"
 git config --global user.email ${GOLLUM_EMAIL}
+
 git init
+if [ ! $REMOTE_REPOSITORY = "no" ]; then
+    git remote add origin $REMOTE_REPOSITORY
+    git pull origin $BRANCH
+fi
 
 gollum --port ${GOLLUM_PORT} --config /config.rb --lenient-tag-lookup
